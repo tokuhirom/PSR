3249f680f0834e81c5f5f4771796f7a234788d62
Tokuhiro Matsuno <tokuhirom@gmail.com>
2014-01-21 07:59:23 +0900

    initial import

---
+++ lib/PSR.pm
@@ -0,0 +1,39 @@
+package PSR;
+use 5.008005;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+PSR - It's new $module
+
+=head1 SYNOPSIS
+
+    use PSR;
+
+=head1 DESCRIPTION
+
+PSR is ...
+
+=head1 LICENSE
+
+Copyright (C) Tokuhiro Matsuno.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Tokuhiro Matsuno E<lt>tokuhirom@gmail.comE<gt>
+
+=cut
+
---
+++ t/00_compile.t
@@ -0,0 +1,9 @@
+use strict;
+use Test::More;
+
+use_ok $_ for qw(
+    PSR
+);
+
+done_testing;
+
